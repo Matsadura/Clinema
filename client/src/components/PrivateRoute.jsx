15be168df32f9de1b65b1a97c1e404b5d5230253
RadouaneAbn <radouaneabn@gmail.com>
2024-09-15 05:24:20 +0100

    [ADD]
    - Created PrivateRoute component to manage route access based on user authentication status.
    - Utilizes DataContext to retrieve and update authentication state (isAuthenticated, setAuth, setUser).
    - Checks token validity by making an API request to /api/auth_validate.
    - Renders component if user is authenticated or redirects to /auth if not.
    - Supports public routes via the "open" prop, allowing unrestricted access when set to true.

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,31 +1,34 @@
-import React, { useEffect, useState, useSyncExternalStore } from "react";
-import { DataConext } from "./Context";
+import React, { useEffect, useState } from "react";
+import { DataContext } from "./Context";
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import { request } from "../tools/requestModule";
 
 export const PrivateRoute = ({ open = false, element: Element }) => {
   const [isLoading, setIsLoading] = useState(true);
-  const { setData } = useContext(DataConext);
-
-  //   localStorage.setItem(
-  //     "_token",
-  //     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcyNjM2MDM3NiwianRpIjoiMGZiNDRkMDYtMDQyYS00OGQ2LTliZjQtY2IyN2FjNTE4NDE2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjlkNGJmNjFlLTgxMzQtNDhlNi1hZjVlLTE5YjI1ZGRhZTg5MCIsIm5iZiI6MTcyNjM2MDM3NiwiY3NyZiI6IjkxZjBkOWUzLTBjNGMtNDI3My1hZjI5LWRlNWI5MWZkNjI3ZiIsImV4cCI6MTcyNjQ0Njc3Nn0.kLdUiEWaYnWGietH3MLkHdN74w929-QVS85TKPuVI-0"
-  //   );
+  const { setAuth, setUser, isAuthenticated } = useContext(DataContext);
+  const token = localStorage.getItem("_token");
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/api/auth_validate", {
-      headers: {
-        Authorization: localStorage.getItem("_token"),
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => setData({ isAuthenticated: true, user: data }))
-      .catch((err) => {
-        console.log(err);
-        setData({ isAuthenticated: false, user: null });
+    request("/auth_validate", headers)
+      .then((res) => {
+        if (res.status === 200) {
+          setUser(res.data);
+          setAuth(true);
+        } else {
+          setAuth(false);
+          setUser(null);
+        }
       })
-      .finally(() => setIsLoading(false));
-  });
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
   if (isLoading) return null;
 
@@ -33,5 +36,5 @@ export const PrivateRoute = ({ open = false, element: Element }) => {
     return <Element />;
   }
 
-  return isAuthenticated ? <Element /> : <Navigate to="/login" />;
+  return isAuthenticated ? <Element /> : <Navigate to="/auth" />;
 };
