e045f0b951701e1bba5a7532be622aeae031759b
RadouaneAbn <radouaneabn@gmail.com>
2024-09-15 05:08:09 +0100

    [ADD] ContextProvider
    This update wraps the app component in a ContextProvider to enable global state sharing across components, improving state handling.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import App from "./App.jsx";
 import "./index.css";
 import HomePage from "./scenes/HomePage.jsx";
 import AllMoviesPage from "./scenes/AllMoviesPage.jsx";
 import { ContextProvider } from "./components/Context.jsx";
 import { PrivateRoute } from "./components/PrivateRoute.jsx";
+import AuthPage from "./scenes/AuthPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <App />
@@ -14,12 +14,13 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route
           path="/"
-          element={<PrivateRoute open={true} component={HomePage} />}
+          element={<PrivateRoute open={true} element={HomePage} />}
         />
         <Route
           path="/AllMovies"
-          element={<PrivateRoute open={true} component={AllMoviesPage} />}
+          element={<PrivateRoute open={true} element={AllMoviesPage} />}
         />
+        <Route path="auth" element={<AuthPage />} />
       </Routes>
     </Router>
   </ContextProvider>
