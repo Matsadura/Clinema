01f86b24af6f8fc0303a4d78b56d09c5401a70c6
RadouaneAbn <radouaneabn@gmail.com>
2024-09-15 05:47:35 +0100

    [CLEANING]

--- client/src/scenes/HomePage.jsx
+++ client/src/scenes/HomePage.jsx
@@ -3,19 +3,18 @@ import HeroSection from "../components/HeroSection.jsx";
 import LocationComponent from "../components/LocationComponent.jsx";
 import MovieList from "../components/MovieList.jsx";
 import Moods from "../components/moods.jsx";
-import MovieCard from "../components/MovieCard.jsx";
 
 const HomePage = () => {
-    return (
-        <div>
-            <Navbar/>
-            <HeroSection/>
-            <LocationComponent />
-            <MovieList/>
-            <Moods/>
-            {/*<MovieCard/>*/}
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <LocationComponent />
+      <MovieList />
+      <Moods />
+      {/*<MovieCard/>*/}
+    </div>
+  );
+};
 
 export default HomePage;
