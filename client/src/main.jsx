427bb03193c13f82497834ed5154b2a7ef025611
oumaima <oumaimanaanaapro@gmail.com>
2024-09-16 20:21:36 +0100

    [ADD] Liked and Saved movie Pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,9 +8,10 @@ import { PrivateRoute } from "./components/PrivateRoute.jsx";
 import AuthPage from "./scenes/AuthPage.jsx";
 import About from "./scenes/About.jsx";
 import UserProfile from "./scenes/UserProfile.jsx";
+import LikedMoviesPage from "./scenes/LikedMoviesPage.jsx";
+import SavedMoviesPage from "./scenes/SavecMoviesPage.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <App />
   <ContextProvider>
     <Router>
       <Routes>
@@ -18,10 +19,12 @@ createRoot(document.getElementById("root")).render(
           path="/"
           element={<PrivateRoute open={true} element={HomePage} />}
         />
-          <Route path="/AllMovies"  element={<PrivateRoute open={true} element={AllMoviesPage}  />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/users/profile" element={<PrivateRoute element={UserProfile} />} />
-          <Route path="/about" element={<About />} />
+        <Route path="/AllMovies" element={<PrivateRoute open={true} element={AllMoviesPage} />} />    
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/liked-movies" element={<PrivateRoute  element={LikedMoviesPage} />} />
+        <Route path="/saved-movies" element={<PrivateRoute  element={SavedMoviesPage} />} />
+        <Route path="/users/profile" element={<PrivateRoute element={UserProfile} />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   </ContextProvider>
