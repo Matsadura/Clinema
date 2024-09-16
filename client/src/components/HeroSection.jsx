eb4ad95dd84d063a1ff2818dd379fd35ec575ada
ila36IX <jbariali002@gmail.com>
2024-09-16 18:34:44 +0100

    [Update] Responsive hero Section

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,80 +1,65 @@
-export default function Example() {
-	const [mobileNavOpen, setMobileNav] = useState(false);
-	const [isAuth, setIsAuth] = useState(true);
+import { useContext, useEffect } from 'react';
+import './styles/NavBar.css';
+import { DataContext } from "./Context";
+import { useNavigate } from "react-router-dom";
+import { FiChevronRight } from "react-icons/fi";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-	return (
-		<div className="bg-secondary-dark">
-			<div className="text-white">
-				<div className='text-white'>
-					<nav className="flex w-full items-center justify-between px-4 sm:px-6">
-						<div className="navbar bg-secondary-light flex items-center w-full md:w-[80%]">
-							<div className="flex w-full items-center justify-between md:w-auto">
-								<a href="#">
-									<span className="sr-only">Clinema</span>
-									<img
-										className="h-8 w-auto sm:h-10"
-										src={brandImage}
-										alt=""
-									/>
-								</a>
-							</div>
-							<div className="hidden md:space-x-8 space-x-1 md:ml-10 pt-2 md:flex">
-								<a href="#" className="text-base font-medium text-white-light hover:text-primary">Weather</a>
-								<a href="#" className="text-base font-medium text-white hover:text-primary">Mood</a>
-								<a href="#" className="text-base font-medium text-white hover:text-primary">Saved</a>
-								<a href="#" className="text-base font-medium text-white hover:text-primary">Liked</a>
-							</div>
-						</div>
+export default function HeroSection() {
+	const { user } = useContext(DataContext);
+	const navigate = useNavigate();
+	useEffect(() => {
+		AOS.init({
+			disable: "phone",
+			duration: 700,
+			easing: "ease-out-cubic",
+		});
+	}, []);
 
-						{/* Mobile hamburger icon */}
-						<div className='md:hidden hamburger__icon flex justify-center w-[130px] bg-secondary-light items-center'>
-							<button
-								className='border-2 text-1xl rounded-full p-2 hover:text-primary hover:border-primary'
-								onClick={() => setMobileNav(!mobileNavOpen)}
-							>
-								{mobileNavOpen ? <IoCloseSharp /> : <GiHamburgerMenu />}
-							</button>
-						</div>
-
-						{isAuth ? <div className="hidden md:flex justify-end text-sm lg:text-lg items-center gap-2 mr-8">
-							<span className='lg:font-bold border-b-2 border-primary pb-1 text-center px-2'>Hello Alien!</span>
-							<div>
-								<img src={avatarUserImg} className='ml-6 md:ml-0 rounded-full w-12 border-2 border-primary' alt="avatar of the user" />
-							</div>
-						</div>
-							: <div className="hidden self-end md:flex md:items-center md:space-x-6 mr-6">
-								<a href="#" className="w-24 flex justify-center text-base font-medium text-white hover:text-primary hover:rounded-md hover:bg-secondary-light px-4 py-2 border border-transparent hover:border-primary">
-									Sign in
-								</a>
-								<a
-									href="#"
-									className="w-24 inline-flex justify-center items-center rounded-md border border-transparent bg-primary font-bold px-4 py-2 text-base text-white hover:border-white hover:bg-green-900"
-								>
-									Sign up
-								</a>
-							</div>}
-					</nav>
-				</div>
-
-				{/* movile nav */}
-				{mobileNavOpen ?
-					<div className='border-t-4 border-secondary-dark bg-secondary-light flex flex-col items-start p-5'>
-						{isAuth ? <div className="md:hidden flex justify-end text-sm lg:text-lg items-center gap-2 mr-8">
-							<div>
-								<img src={avatarUserImg} className='md:ml-0 rounded-full w-12 border-2 border-primary' alt="avatar of the user" />
-							</div>
-							<span className='font-bold border-b-2 border-primary pb-1 text-center px-2'>Hello Alien!</span>
-						</div> : null}
-						<nav>
-							<div className="md:hidden text-lg flex gap-3 flex-col p-4">
-								<a href="#" className="font-medium text-white-light hover:text-primary">Weather</a>
-								<a href="#" className="font-medium text-white hover:text-primary">Mood</a>
-								<a href="#" className="font-medium text-white hover:text-primary">Saved</a>
-								<a href="#" className="font-medium text-white hover:text-primary">Liked</a>
-							</div>
-						</nav>
-					</div> : null}
+	return <main className='h-screen grid md:grid-cols-2 grid-cols-1 p-10 md:gap-16 items-center lg:py-14 lg:px-24 mt-10 md:mt-0'>
+		<div data-aos="fade-right" className="flex flex-col md:items-start items-center md:text-start text-center">
+			<a
+				href="#location_section"
+				className="inline-flex  items-center rounded-full bg-black p-1 pr-2 text-white hover:text-gray-200 sm:text-sm lg:text-sm xl:text-base"
+			>
+				<span className="rounded-full bg-gradient-to-r from-primary to-green-800 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
+					Your weather
+				</span>
+				<span className="ml-4 text-sm">New movie to watch</span>
+				<FiChevronRight className="ml-2 text-lg" />
+			</a>
+			<h1 className="mt-4 text-4xl font-bold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+				<span className="block">Mood & Weather</span>
+				<span className="block bg-gradient-to-r from-green-100 to-primary bg-clip-text pb-3 text-transparent sm:pb-5">
+					Your Movie Match.
+				</span>
+			</h1>
+			<p className="text-base text-gray-300 sm:text-xl md:p-0 px-5 lg:text-lg xl:text-xl">
+				Find your perfect movie! We use your local weather and mood to suggest films you'll love. Let's start watching!
+			</p>
+		</div>
+		<div className="h-full">
+			{/* clt for mobile */}
+			{user ? null : <div className="flex self-end md:hidden gap-4 justify-center my-10 items-center">
+				<a onClick={() => navigate('/auth')} className="text-base font-medium text-white hover:text-primary rounded-md hover:bg-secondary-light px-4 py-2 border border-white	 hover:border-primary">
+					Sign in
+				</a>
+				<a
+					href="#"
+					className="inline-flex items-center rounded-md border border-transparent bg-primary font-bold px-4 py-2 text-base text-white hover:border-white hover:bg-green-900"
+					onClick={() => navigate('/auth')}
+				>
+					Sign up
+				</a>
+			</div>}
+			<div data-aos="fade-left" className="max-w-md bg-cover md:h-full flex justify-center  md:px-5 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-5">
+				<img
+					className="md:w-full w-1/2 lg:inset-y-0 md:p-8 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+					src="https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg"
+					alt=""
+				/>
 			</div>
 		</div>
-	)
+	</main>
 }
